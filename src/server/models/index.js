f77b8450e75677f5c9e685d17bbbf81269db5c3a
Simon Kort <simon.kort@gmail.com>
2015-05-26 09:06:41 +0200

    Promises working on testdata

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -25,20 +25,22 @@ db.Scenario = Scenario;
 
 module.exports = db;
 
-//sequelize.sync({force: true}).then(function() {
-//	Scenario.create({ 
-//		name: "Silent Tide", 
-//		description: "When strange reports of misty undead spread through Absalom, you and your fellow Pathfinders are dispatched to the half-drowned district of Puddles. Notoriously rough, the drooling addicts, flesh panderers, and quick-handed knifers of Puddles are the least of your worries. The night's tide brings with it an ancient armada of some long-forgotten war and you are the only thing between their mist-shrouded ghost fleet and Absalom's utter oblivion.",
-//		season: "0",
-//		number: "1",
-//		tier: "1-5"
-//	});
-//
-//	Scenario.create({ 
-//		name: "The Hydra's Fang Incident", 
-//		description: "After an Andoren village is razed by the Hydra's Fang, a renegade Chelish slaver-ship, outrage threatens the stability of both nations. You and your fellow Pathfinders are sent to capture the Fang before the Inner Sea is pitched into political frenzy.",
-//		season: "0",
-//		number: "2",
-//		tier: "1-5"
-//	});
-//});
+sequelize.sync({force: true}).then(function() {
+	var data = [
+               	Scenario.create({ 
+               		name: "Silent Tide", 
+               		description: "When strange reports of misty undead spread through Absalom, you and your fellow Pathfinders are dispatched to the half-drowned district of Puddles. Notoriously rough, the drooling addicts, flesh panderers, and quick-handed knifers of Puddles are the least of your worries. The night's tide brings with it an ancient armada of some long-forgotten war and you are the only thing between their mist-shrouded ghost fleet and Absalom's utter oblivion.",
+               		season: "0",
+               		number: "1",
+               		tier: "1-5"
+               	}),
+               	Scenario.create({ 
+               		name: "The Hydra's Fang Incident", 
+               		description: "After an Andoren village is razed by the Hydra's Fang, a renegade Chelish slaver-ship, outrage threatens the stability of both nations. You and your fellow Pathfinders are sent to capture the Fang before the Inner Sea is pitched into political frenzy.",
+               		season: "0",
+               		number: "2",
+               		tier: "1-5"
+               	})	            
+	            ];
+	Sequelize.Promise.all(data);
+});
