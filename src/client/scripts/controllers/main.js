e84f4258af80fdf4f24e1705d4b0fff02aef77ca
Nieskel <n.a.lindelauf-denheijer@uu.nl>
2015-06-24 15:57:26 +0200

    added a people-model and the api requests to fetch all people or one person by id. No database change yet.

--- src/client/scripts/controllers/main.js
+++ src/client/scripts/controllers/main.js
@@ -11,6 +11,8 @@
     	vm.$http = $http;
     	vm.scenarios = [];
     	vm.getScenarios() ; 
+    	vm.people = [];
+    	vm.getPeople() ;
     }
     
     MainController.prototype.getScenarios = function()
@@ -21,6 +23,22 @@
     	    // this callback will be called asynchronously
     	    // when the response is available
     		  vm.scenarios = data;
+    	  }).
+    	  error(function(data, status, headers, config) {
+    	    // called asynchronously if an error occurs
+    	    // or server returns response with an error status.
+    		  console.log('ERROR!');
+    	  });
+    }
+    
+    MainController.prototype.getPeople = function()
+    {
+    	var vm = this;
+    	vm.$http.get('http://localhost/pfs-scenariotracker/src/server_ci3/index.php/api/v1/people').
+    	  success(function(data, status, headers, config) {
+    	    // this callback will be called asynchronously
+    	    // when the response is available
+    		  vm.people = data;
     		  console.log (data);
     	  }).
     	  error(function(data, status, headers, config) {
