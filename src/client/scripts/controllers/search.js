0a4296055ed1794f44868f9e71e83d03e82df4f4
Simon Kort <simon.kort@gmail.com>
2015-06-26 15:21:10 +0200

    Basic client work and more DB data

--- src/client/scripts/controllers/search.js
+++ src/client/scripts/controllers/search.js
@@ -3,9 +3,9 @@
 
     angular
         .module('scenariotracker')
-        .controller('MainController', MainController );
+        .controller('SearchController', SearchController );
     
-    function MainController($http)
+    function SearchController($http)
     {
     	var vm = this;
     	vm.$http = $http;
@@ -13,7 +13,7 @@
     	vm.getScenarios() ; 
     }
     
-    MainController.prototype.getScenarios = function()
+    SearchController.prototype.getScenarios = function()
     {
     	var vm = this;
     	vm.$http.get('http://localhost/pfs-scenariotracker/src/server_ci3/index.php/api/v1/scenarios').
@@ -21,12 +21,11 @@
     	    // this callback will be called asynchronously
     	    // when the response is available
     		  vm.scenarios = data;
-    		  console.log (data);
     	  }).
     	  error(function(data, status, headers, config) {
     	    // called asynchronously if an error occurs
     	    // or server returns response with an error status.
-    		  console.log('ERROR!');
+    		  console.log('ERROR loading scenarios');
     	  });
     }
     
