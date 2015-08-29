59da431075017517cf9fed5aa1a5883b77e22cd3
NieskeL <n.a.lindelauf-denheijer@uu.nl>
2015-08-29 20:55:57 +0200

    made the layout better. Added the checkbox and some text. Also, the submit button now puts an array of values in the console.log

--- src/client/scripts/controllers/register.js
+++ src/client/scripts/controllers/register.js
@@ -14,11 +14,16 @@
     	vm.playeremail = "";
     	vm.playerpassword = "";
     	vm.checkpassword = "";
+    	vm.othersmayreport = false;
     }
     
     RegisterController.prototype.savePlayer = function()
     {
-    	console.log('ik werk!');
+   	
+    	var vm = this;
+    	var person = {email : vm.playeremail, password : vm.playerpassword, name : vm.playername, pfsnumber : vm.playernumber, report : vm.othersmayreport};
+    	
+    	console.log(person);
     }
     
 })();
