abe55925f2b321d397016db96e5c9c295831d924
NieskeL <n.a.lindelauf-denheijer@uu.nl>
2015-09-09 21:21:06 +0200

    the view works nicely now! It checks if the passwords match, if the password is between 8 and 20 characters and all the fields are there. Sadly, the ap and I do not see eye to eye yet

--- src/client/scripts/controllers/register.js
+++ src/client/scripts/controllers/register.js
@@ -9,6 +9,7 @@
     {
     	var vm = this;
     	vm.$state = $state;
+    	vm.$http = $http;
     	vm.playername = "";
     	vm.playernumber = "";
     	vm.playeremail = "";
@@ -21,9 +22,30 @@
     {
    	
     	var vm = this;
-    	var person = {email : vm.playeremail, password : vm.playerpassword, name : vm.playername, pfsnumber : vm.playernumber, report : vm.othersmayreport};
+    	var person = {email : vm.playeremail, password : vm.playerpassword, name : vm.playername, pfsnumber : vm.playernumber, public : vm.othersmayreport};
     	
     	console.log(person);
-    }
+
+//      	var method = 'POST';
+//      	
+//      	var req = {
+//                method: method,
+//                url: 'http://pfs.campaigncodex.com/api/v1/person',
+//                data: $.param({pfsnumber : vm.playernumber, name : vm.playername, password : vm.playerpassword, public : vm.othersmayreport, email : vm.playeremail}),
+//                headers: {
+//                    'Content-Type': 'application/x-www-form-urlencoded'
+//                }
+//            };    	
+//    	
+//    	vm.$http(req).
+//			success(function(data, status, headers, config) {
+//		}).
+//	  	error(function(data, status, headers, config) {
+//	  		// called asynchronously if an error occurs
+//	  		// or server returns response with an error status.
+//	  		console.log('ERROR saving content');
+//	  	});
+       
+       }
     
 })();
