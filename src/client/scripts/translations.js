236f0c2debcbd1fcdf6a001b77e59a76566b2935
FlavienKnuchel <knuchelf@gmail.com>
2016-01-12 19:35:47 +0100

    steeing all translations in the view, re-rendering .pot

--- src/client/scripts/translations.js
+++ src/client/scripts/translations.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('fr', {"About":"À propos","Are you sure?":"êtes-vous sûr?","Report":"Rapporter"});
+    gettextCatalog.setStrings('fr', {"About":"À propos","Are you sure?":"ètes-vous sûr?","Report":"Rapporter"});
 /* jshint +W100 */
 }]);
